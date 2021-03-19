7e80d2f2cf227209f113937f6c5efc270397514d
winljm001 <819648983@qq.com>
2021-03-19 18:03:38 +0800

    init

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+const path = require('path');
+const fs = require('fs');
+const defaultConfig = require('./config.json');
 import * as vscode from 'vscode';
+import { main } from './main';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,11 +15,36 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('generate-lang-vscode.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('generate-lang-vscode.generateLang', (uri) => {
 		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from generate-lang-vscode!');
+		// 读取ts文件
+		const langPath=uri?.fsPath;
+		const workspaceFolders=vscode.workspace?.workspaceFolders;
+		let workspacePath='./';
+		if(workspaceFolders&&workspaceFolders?.length>0){
+			const {uri}= workspaceFolders[0];
+			workspacePath=uri?.fsPath;
+		}
+		const CONFIG_PATH = path.normalize(path.join(workspacePath,'./generate-lang-config.json'));
+		
+		let config={...defaultConfig,mainLangPath:path.normalize(langPath)};
+		try {
+			 config = JSON.parse(fs.readFileSync(CONFIG_PATH,'utf-8'));
+		} catch (error) {
+			try {
+				const workspaceFolders=vscode.workspace?.workspaceFolders;
+				if(workspaceFolders&&workspaceFolders?.length>0){
+					const {uri}= workspaceFolders[0];
+					// 没有配置文件生成配置文件
+					fs.writeFileSync(`${uri?.fsPath}/generate-lang-config.json`,JSON.stringify(config));
+				}
+			} catch (error) {
+				console.log(error);	
+			}
+		}
+		main(config);
+		// // Display a message box to the user
+		// vscode.window.showInformationMessage(langPath);
 	});
 
 	context.subscriptions.push(disposable);
---
+++ src/utils.ts
@@ -0,0 +1,152 @@
+const xlsx = require('node-xlsx');
+const fs = require('fs');
+const prettier = require('prettier');
+
+const prettierOptions = {
+  singleQuote: true,
+  semi: false,
+};
+// 数组去重
+export const unique = (arr: any[]) => {
+  return Array.from(new Set(arr));
+};
+
+// 从对象中获取所有的值
+export const getAllwords = (langObj: any, parentKey = "") => {
+  let resList: any[] = [];
+  for (var index in langObj) {
+    const item = langObj[index];
+    if (typeof item === "string") {
+      resList.push({ key: parentKey + "." + index, value: item });
+    } else {
+      resList = resList.concat(getAllwords(item, parentKey + "." + index));
+    }
+  }
+  return resList;
+};
+
+// 读取主语言的文件
+export const readFileList = (fullPath: any) => {
+  
+  return new Promise((resolve, reject) => {
+    if (!fullPath) {
+      reject();
+    }
+    // const res = fs.readFileSync(fullPath,"utf-8");
+    console.log('fullPath',fullPath);
+    // const res= require(fullPath);
+    // console.log(res);
+    import(fullPath).then((res) => {
+      resolve(res);
+    });
+  });
+};
+
+// 读取Excel
+export const getImportExcel = (importExcelPath: string) => {
+  try {
+    const workSheetsFromBuffer = xlsx.parse(fs.readFileSync(importExcelPath));
+    const data = workSheetsFromBuffer[0]?.data;
+    const resData: any[] = [];
+    if (data.length > 1) {
+      for (let i = 1; i < data[0].length; i++) {
+        const name = data[0][i];
+        resData.push({ name: name, data: [] });
+      }
+    } else {
+      throw new Error("xlsx内格式不对");
+    }
+    for (let i = 1; i < data.length; i++) {
+      for (let j = 1; j <= resData.length; j++) {
+        const word = data[i][j];
+        resData[j - 1]?.data?.push({
+          key: data[i][0],
+          value: word ? word : "",
+        });
+      }
+    }
+    return resData;
+  } catch (error) {
+    throw new Error("读取xlsx错误，或者xlsx内格式不对");
+  }
+};
+
+// 生成Excel文件的数据
+
+export const getExportExcelData = (data: any) => {
+  let res: any = [];
+  const titleRow: any[] = [""];
+  const dataList: any[] = [];
+  data.forEach((v: any) => {
+    titleRow.push(v?.name);
+  });
+  data[0].data.forEach((v: any, i: any) => {
+    const tempItem: any[] = [v?.key];
+    data.forEach((item: any, j: any) => {
+      tempItem[j + 1] = item?.data?.find((keyItem:any)=>{
+        return keyItem?.key===v?.key;
+      })?.value;
+    });
+    dataList.push(tempItem);
+  });
+  res.push(titleRow);
+  res = res.concat(dataList);
+  const resData = [{ name: "sheet1", data: res }];
+  return resData;
+};
+
+// 导出excel
+export const writeExcel = (name: any, data: any) => {
+  try {
+    if (!fs.existsSync("./excel")) {
+      fs.mkdirSync("./excel");
+    }
+    const buffer: any = xlsx.build(data);
+    fs.writeFileSync("./excel/" + name + ".xlsx", buffer, { flag: "w" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const replaceLang = (obj: any, originData: any, parentKey = "") => {
+  const resObj = JSON.parse(JSON.stringify(obj));
+  for (var index in resObj) {
+    const item = resObj[index];
+    if (typeof item === "string") {
+      const findWord = originData.find((v: any) => {
+        return v.key === parentKey + "." + index;
+      });
+      resObj[index] = findWord?.value;
+    } else {
+      resObj[index] = replaceLang(item, originData, parentKey + "." + index);
+    }
+  }
+  return resObj;
+};
+// 多语言文件生成
+export const generateJSFiles = (
+  mainLang: any,
+  importExcelData: any,
+  outLang: any
+) => {
+  outLang?.forEach((v: any) => {
+    const originData = importExcelData.find((item: any) => {
+      return item?.name === v;
+    })?.data;
+    writeTs(v, replaceLang(mainLang, originData));
+  });
+};
+
+// 生成ts
+export const writeTs = (name: any, data: any) => {
+  try {
+    if (!fs.existsSync("./dist")) {
+      fs.mkdirSync("./dist");
+    }
+    const scriptContent = `export default ${JSON.stringify(data)}`;
+    const prettierContent = prettier.format(scriptContent, prettierOptions);
+    fs.writeFileSync("./dist/" + name + ".ts", prettierContent);
+  } catch (error) {
+    console.log(error);
+  }
+};
